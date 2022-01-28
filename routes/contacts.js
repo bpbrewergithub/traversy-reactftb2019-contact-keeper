1d8d5a9e70de10e28f654a6ddb621967e4c202c3
Dev <dev@email.com>
2022-01-28 16:49:25 +1000

    Up to and including lecture 47

--- routes/contacts.js
+++ routes/contacts.js
@@ -3,12 +3,29 @@ const express = require('express');
 // Store the express.Router module as a variable
 const router = express.Router();
 
+// Import require the 'auth' middleware
+const auth = require('../middleware/auth');
+
+// Import/require validator modules
+const { check, validationResult } = require('express-validator');
+
+// Import/require the User model
+const User = require('../models/User');
+// Import/require the Contact model
+const Contact = require('../models/Contact');
+
 // @route            GET api/contacts
 // @description      Get all user contacts
 // @access           Private
 
-router.get('/', (req, res) => {
-  res.send('Get all user contacts');
+router.get('/', auth, async (req, res) => {
+  try {
+    const contacts = await Contact.find({ user: req.user.id }).sort({ date: -1 });
+    res.json(contacts);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send('Server error');
+  }
 });
 
 // @route            POST api/contacts
