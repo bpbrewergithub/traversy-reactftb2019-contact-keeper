36b18138c1a5eeb349a91373b3a51dd8788c646e
Dev <dev@email.com>
2022-02-08 14:35:12 +1000

    Up to and including lecture 55

--- client/src/context/contact/contactReducer.jsx
+++ client/src/context/contact/contactReducer.jsx
@@ -7,15 +7,23 @@ import {
   CLEAR_FILTER
 } from '../types'
 
-export default (state, action) => {
+const reducer = (state, action) => {
   switch(action.type) {
     case ADD_CONTACT:
       return {
         ...state,
         contacts: [...state.contacts, action.payload]
+      };
+
+    case DELETE_CONTACT:
+      return {
+        ...state,
+        contacts: state.contacts.filter(contact => contact.id !== action.payload)
       }
 
     default:
       return state;
   }
 }
+
+export default reducer;
