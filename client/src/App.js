6eb11a6aa99b7a23981c0244bd9594912f9de8d0
Dev <dev@email.com>
2022-02-08 17:47:10 +1000

    Up to and including lecture 62

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,8 @@
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 // Component imports
-import Navbar from './components/layout/Navbar'
+import Navbar from './components/layout/Navbar';
+import Alerts from './components/layout/Alerts';
 import Home from './components/pages/Home';
 import About from './components/pages/About';
 import Register from './components/auth/Register';
@@ -10,7 +11,8 @@ import Login from './components/auth/Login';
 
 // Context/State imports
 import ContactState from './context/contact/ContactState';
-import AuthState from './context/auth/AuthState'
+import AuthState from './context/auth/AuthState';
+import AlertState from './context/alert/AlertState';
 
 // Style imports
 import './App.css';
@@ -19,19 +21,22 @@ const App = () => {
   return (
     <AuthState>
       <ContactState>
-        <Router>
-          <>
-            <Navbar />
-            <div className="container">
-              <Switch>
-                <Route exact path='/' component={Home} />
-                <Route exact path='/about' component={About} />
-                <Route exact path='/register' component={Register} />
-                <Route exact path='/login' component={Login} />
-              </Switch>
-            </div>
-          </>
-        </Router>
+        <AlertState>
+          <Router>
+            <>
+              <Navbar />
+              <div className="container">
+                <Alerts />
+                <Switch>
+                  <Route exact path='/' component={Home} />
+                  <Route exact path='/about' component={About} />
+                  <Route exact path='/register' component={Register} />
+                  <Route exact path='/login' component={Login} />
+                </Switch>
+              </div>
+            </>
+          </Router>
+        </AlertState>
       </ContactState>
     </AuthState>
   );
