df3c3d202dc5f1165d2b8e7e9f214070a8e0af14
Dev <dev@email.com>
2022-02-08 12:53:42 +1000

    Up to and including lecture 52

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,32 @@
+// Module imports
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+// Component imports
 import Navbar from './components/layout/Navbar'
 import Home from './components/pages/Home';
 import About from './components/pages/About';
+
+// Context/State imports
+import ContactState from './context/contact/ContactState';
+
+// Style imports
 import './App.css';
 
 const App = () => {
   return (
-    <Router>
-      <>
-        <Navbar />
-        <div className="container">
-          <Switch>
-            <Route exact path='/' component={Home} />
-            <Route exact path='/about' component={About} />
-          </Switch>
-        </div>
-      </>
-    </Router>
+    <ContactState>
+      <Router>
+        <>
+          <Navbar />
+          <div className="container">
+            <Switch>
+              <Route exact path='/' component={Home} />
+              <Route exact path='/about' component={About} />
+            </Switch>
+          </div>
+        </>
+      </Router>
+    </ContactState>
   );
 }
 
